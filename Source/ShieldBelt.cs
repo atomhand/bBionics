33bb508e296c4fb21b66e5832ba1fccbcfb1f26d
Tom Kellett <thessalikon@live.co.uk>
2020-07-27 23:39:00 +0100

    initial commit

---
+++ Source/ShieldBelt.cs
@@ -0,0 +1,34 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace bBionics
+{
+    static class ShieldBeltUtility
+    {
+        public static float EnergyMax( ShieldBelt me, StatDef stat = null, bool b = false)
+        {
+            Pawn wearer = me.Wearer;
+            float result = me.GetStatValue(StatDefOf.EnergyShieldEnergyMax, true);
+            if( wearer != null )
+                result *= wearer.GetStatValueForPawn(BStatDefOf.EnergyShieldEnergyMaxMultiplier, wearer);
+
+            return result;
+        }
+
+        public static float EnergyGainPerTick( ShieldBelt me)
+        {
+            Pawn wearer = me.Wearer;
+            float result = me.GetStatValue(StatDefOf.EnergyShieldRechargeRate, true) / 60f;
+            if (wearer != null)
+                result *= wearer.GetStatValueForPawn(BStatDefOf.EnergyShieldRechargeRateMultiplier, wearer);
+
+            return result;
+        }
+    }
+}
---
+++ Source/StatDefOf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+
+namespace bBionics
+{
+    [DefOf]
+    public static class BStatDefOf
+    {
+        public static StatDef EnergyShieldEnergyMaxMultiplier;
+        public static StatDef EnergyShieldRechargeRateMultiplier;
+
+        static BStatDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(StatDefOf));
+        }
+    }
+}
